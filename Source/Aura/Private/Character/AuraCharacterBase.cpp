a250f1241efe46dd780c2edb2b2b714036dd8952
nooburger81 <brandonsebastian@gmail.com>
2025-07-31 12:29:31 -0400

    organizing files

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright ChefDevGaming 2025
+
+
+#include "AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
